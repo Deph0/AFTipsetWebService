2beb84cbfc00f65c917166042ac4e766de70b6ae
Deph0 <Deph0@users.noreply.github.com>
2018-06-18 11:30:03 +0200

    Hotfixed vmresult.js - forgot path

--- VMTipsetWebAPI/routes/vmresults.js
+++ VMTipsetWebAPI/routes/vmresults.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const fs = require('fs')
 const router = express.Router()
 const database = require('../database')
@@ -6,7 +7,7 @@ const database = require('../database')
 router.route('/')
   // get all the files (accessed at GET http://localhost:8080/api/v1/result)
   .get(function (req, res) {
-    const sql = fs.readFileSync('../sql/all_results.sql', 'utf8').toString()
+    const sql = fs.readFileSync(path.join(__dirname, '../sql', 'all_results.sql'), 'utf8').toString()
     database.query(sql, (err, results, fields) => {
       if (err) {
        // If there is error, we send the error in the error section with 500 status
@@ -20,7 +21,7 @@ router.route('/')
 
 router.route('/matches')
   .get((req, res) => {
-    const sql = fs.readFileSync('../sql/all_matches.sql', 'utf8').toString()
+    const sql = fs.readFileSync(path.join(__dirname, '../sql', 'all_matches.sql'), 'utf8').toString()
     database.query(sql, (err, results, fields) => {
       if (err) {
         // If there is error, we send the error in the error section with 500 status
@@ -34,7 +35,7 @@ router.route('/matches')
 
 router.route('/teams')
   .get((req, res) => {
-    const sql = fs.readFileSync('../sql/all_teams.sql', 'utf8').toString()
+    const sql = fs.readFileSync(path.join(__dirname, '../sql', 'all_teams.sql'), 'utf8').toString()
     database.query(sql, (err, results, fields) => {
       if (err) {
         // If there is error, we send the error in the error section with 500 status
@@ -48,7 +49,7 @@ router.route('/teams')
 
 router.route('/scorers')
   .get((req, res) => {
-    const sql = fs.readFileSync('../sql/all_scorers.sql', 'utf8').toString()
+    const sql = fs.readFileSync(path.join(__dirname, '../sql', 'all_scorers.sql'), 'utf8').toString()
     database.query(sql, (err, results, fields) => {
       if (err) {
         // If there is error, we send the error in the error section with 500 status
